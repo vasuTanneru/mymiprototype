91563251567ea5d1bc57fe2cc1423415752edb51
admin@vasu <vasutanneru035@gmail.com>
2023-03-20 22:12:51 +0530

    First Commit Code

---
+++ script.js
@@ -0,0 +1,119 @@
+function logoEffect()
+{
+    document.getElementById("effect").style.transform="translateX(-8em)";
+    document.getElementById("effect").style.transition="all 0.3s ease 0s,visibility 0s linear 0.3s , z-index 0s linear 0.1s";
+}
+function logoEffect1()
+{
+    document.getElementById("effect").style.transform="translateX(0)";
+    document.getElementById("effect").style.transition="all 0.3s ease 0s,visibility 0s linear 0.3s , z-index 0s linear 0.1s";
+}
+function move11(){
+    document.getElementById("Phone").style.visibility = "visible";
+    document.getElementById("Phone").style.opacity = "1";
+    document.getElementById("Phone").style.zIndex = "1";
+    document.getElementById("Phone").style.transform = "translateY(3em)";
+    document.getElementById("Phone").style.transitionDelay = "0s,0s,0.3s";
+}
+function move12(){
+    document.getElementById("Phone").style.visibility = "hidden";
+    document.getElementById("Phone").style.opacity = "0";
+    document.getElementById("Phone").style.zIndex = "0";
+    document.getElementById("Phone").style.transform = "translateY(-5em)";
+    document.getElementById("Phone").style.transitionDelay = "0s,0s,0.3s";
+}
+function move21(){
+    document.getElementById("Tv").style.visibility = "visible";
+    document.getElementById("Tv").style.opacity = "1";
+    document.getElementById("Tv").style.zIndex = "1";
+    document.getElementById("Tv").style.transform = "translateY(4em)";
+    document.getElementById("Tv").style.transitionDelay = "0s,0s,0.3s";
+}
+function move22(){
+    document.getElementById("Tv").style.visibility = "hidden";
+    document.getElementById("Tv").style.opacity = "0";
+    document.getElementById("Tv").style.zIndex = "0";
+    document.getElementById("Tv").style.transform = "translateY(-5em)";
+    document.getElementById("Tv").style.transitionDelay = "0s,0s,0.3s";
+}
+function move31(){
+    document.getElementById("LaptopsTabs").style.visibility = "visible";
+    document.getElementById("LaptopsTabs").style.opacity = "1";
+    document.getElementById("LaptopsTabs").style.zIndex = "1";
+    document.getElementById("LaptopsTabs").style.transform = "translateY(4em)";
+    document.getElementById("LaptopsTabs").style.transitionDelay = "0s,0s,0.3s";
+}
+function move32(){
+    document.getElementById("LaptopsTabs").style.visibility = "hidden";
+    document.getElementById("LaptopsTabs").style.opacity = "0";
+    document.getElementById("LaptopsTabs").style.zIndex = "0";
+    document.getElementById("LaptopsTabs").style.transform = "translateY(-5em)";
+    document.getElementById("LaptopsTabs").style.transitionDelay = "0s,0s,0.3s";
+}
+function move41(){
+    document.getElementById("LifeStyle").style.visibility = "visible";
+    document.getElementById("LifeStyle").style.opacity = "1";
+    document.getElementById("LifeStyle").style.zIndex = "1";
+    document.getElementById("LifeStyle").style.transform = "translateY(2em)";
+    document.getElementById("LifeStyle").style.transitionDelay = "0s,0s,0.3s";
+}
+function move42(){
+    document.getElementById("LifeStyle").style.visibility = "hidden";
+    document.getElementById("LifeStyle").style.opacity = "0";
+    document.getElementById("LifeStyle").style.zIndex = "0";
+    document.getElementById("LifeStyle").style.transform = "translateY(-5em)";
+    document.getElementById("LifeStyle").style.transitionDelay = "0s,0s,0.3s";
+}
+// function Click1(){
+//     document.getElementById("Discover").style.visibility = "visible";
+//     document.getElementById("Discover").style.opacity = "1";
+//     document.getElementById("Discover").style.zIndex = "1";
+//     document.getElementById("Discover").style.transform = "translateY(5em)";
+//     document.getElementById("Discover").style.transitionDelay = "0s,0s,0.3s";
+
+// }
+// function searchmove1(){
+//     document.getElementById("Signline1").style.visibility = "visible";
+//     document.getElementById("Signline1").style.opacity = "1";
+//     document.getElementById("Signline1").style.zIndex = "1";
+//     document.getElementById("Signline1").style.transform = "translateY(3em)";
+//     document.getElementById("Signline1").style.transitionDelay = "0s,0s,0.3s";
+
+// }
+// function searchmoveout1(){
+//     document.getElementById("Signline1").style.visibility = "hidden";
+//     document.getElementById("Signline1").style.opacity = "0";
+//     document.getElementById("Signline1").style.zIndex = "0";
+//     document.getElementById("Signline1").style.transform = "translateY(-5em)";
+//     document.getElementById("Signline1").style.transitionDelay = "0s,0s,0.3s";
+// }
+function searchmove2(){
+    document.getElementById("searchline2").style.visibility = "visible";
+    document.getElementById("searchline2").style.opacity = "1";
+    document.getElementById("searchline2").style.zIndex = "1";
+    document.getElementById("searchline2").style.transform = "translateY(3em)";
+    document.getElementById("searchline2").style.transitionDelay = "0s,0s,0.3s";
+
+}
+function searchmoveout2(){
+    document.getElementById("searchline2").style.visibility = "hidden";
+    document.getElementById("searchline2").style.opacity = "0";
+    document.getElementById("searchline2").style.zIndex = "0";
+    document.getElementById("searchline2").style.transform = "translateY(-5em)";
+    document.getElementById("searchline2").style.transitionDelay = "0s,0s,0.3s";
+
+}
+function Signlinein(){
+    document.getElementById("Signline1").style.visibility = "visible";
+    document.getElementById("Signline1").style.opacity = "1";
+    document.getElementById("Signline1").style.zIndex = "1";
+    document.getElementById("Signline1").style.transform = "translateY(3em)";
+    document.getElementById("Signline1").style.transitionDelay = "0s,0s,0.3s";
+}
+function Signlineout(){
+    document.getElementById("Signline1").style.visibility = "hidden";
+    document.getElementById("Signline1").style.opacity = "0";
+    document.getElementById("Signline1").style.zIndex = "0";
+    document.getElementById("Signline1").style.transform = "translateY(-5em)";
+    document.getElementById("Signline1").style.transitionDelay = "0s,0s,0.3s";   
+}
